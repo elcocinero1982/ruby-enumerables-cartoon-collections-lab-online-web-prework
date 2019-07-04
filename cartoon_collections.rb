4fec2b1e4afcdda539fad36e8f10e67a4ba39c87
jose luis <cortezluisjose@gmail.com>
2019-07-04 23:19:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  roll_call_dwarves[1 => Doc, 2 => Dopet, 3 => Bashful, 4 => Grumpy]
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  summon_captain_planet = jose_planets
 end
 
 def long_planeteer_calls# code an argument here
